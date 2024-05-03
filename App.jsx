fa6e04a67abeff14b870ff5b0c0c6823fd40e174
Hanzamune69 <127651010+Hanzamune69@users.noreply.github.com>
2024-05-03 12:34:03 +0800

    react-js

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-xl font-bold text-red-800'>Hello Tailwindcss with React Vite</h1>
+    </>
+  )
+}
+
+export default App
